96db5c1e159be50bbc5ef226d8c76140db1e05c2
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-09-23 18:25:58 +0300

    MainSection: Edited and added some comments

--- src/renderer/src/components/MainSection.jsx
+++ src/renderer/src/components/MainSection.jsx
@@ -4,9 +4,12 @@ export default function MainSection() {
       <div className="flex h-full p-1">
         <div className="bg-ctp-surface0 m-1 basis-1/4">
           <div className="flex flex-col p-1 h-full">
+            {/* Send Sequences */}
             <div className="bg-ctp-surface1 m-1 flex-1">
               <h1 className="text-center text-ctp-text">3.1.1</h1>
             </div>
+
+            {/* Recieve Sequences */}
             <div className="bg-ctp-surface1 m-1 flex-1">
               <h1 className="text-center text-ctp-text">3.1.2</h1>
             </div>
@@ -15,11 +18,9 @@ export default function MainSection() {
 
         <div className="bg-ctp-surface0 m-1 basis-3/4">
           <div className="flex flex-col p-1 h-full">
+            {/* Communication Window */}
             <div className="bg-ctp-surface1 m-1 flex-1">
-              <h1 className="text-center text-ctp-text">3.2.1</h1>
-            </div>
-            <div className="bg-ctp-surface1 m-1 basis-1/4">
-              <h1 className="text-center text-ctp-text">3.2.2</h1>
+              <h1 className="text-center text-ctp-text">3.2</h1>
             </div>
           </div>
         </div>
