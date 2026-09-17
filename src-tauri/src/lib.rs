9337be1075cd898491129bc09f93b7396ac47bc4
Muhammed Emin Boydak <me@eminboydak.com>
2026-09-17 15:10:45 +0300

    fix(ci): add tauri-plugin-fs to Cargo.toml + register in builder
    - Cargo.toml: added tauri-plugin-fs dependency
    - lib.rs: registered tauri_plugin_fs::init()
    - capabilities: fs:default + fs:read-file permissions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -98,6 +98,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_serialport::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .manage(app_state.clone())
         .invoke_handler(tauri::generate_handler![
             list_ports,
